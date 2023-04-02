8519942a675bfab4123415726d26872f16fd141f
Vladyslav Klymiuk <vladyslav.klymiuk@student.tuke.sk>
2023-04-02 17:33:45 +0200

    Replace Lexer.java

--- zadanie-2/src/Lexer.java
+++ zadanie-2/src/Lexer.java
@@ -1,84 +1,81 @@
+import java.io.IOException;
+import java.io.Reader;
+
 public class Lexer {
     private int current;
     private int value;
-    private final String input;
-    private final int inputLength;
+    private final Reader reader;
 
-    public Lexer(String input)
+    public Lexer(Reader reader)
     {
-        this.input = input;
-        this.current = 0;
-        this.inputLength = input.length();
+        this.reader = reader;
+        consume();
     }
 
     public Token nextToken()
     {
-        if(current < inputLength)
-        {
-            while (Character.isWhitespace(input.charAt(current)))
-            {
-                consume();
-            }
-
-            switch (input.charAt(current))
-            {
-                case '+':
-                    consume();
-                    return Token.PLUS;
-
-                case '-':
-                    consume();
-                    return Token.MINUS;
-
-                case '*':
-                    consume();
-                    return Token.MUL;
-
-                case '/':
-                    consume();
-                    return Token.DIV;
-
-                case '^':
-                    consume();
-                    return Token.Degree;
-
-                case '⊗':
-                {
-                    consume();
-                    return Token.MUL;
-                }
-
-                case '(':
-                    consume();
-                    return Token.LPAR;
-
-                case ')':
-                    consume();
-                    return Token.RPAR;
-            }
-
-            if (Character.isDigit(input.charAt(current)))
-            {
-                StringBuilder sb = new StringBuilder();
-                do
-                {
-                    sb.append(input.charAt(current));
-                    consume();
-                }
-                while (current < inputLength && Character.isDigit(input.charAt(current)));
-                value = Integer.parseInt(sb.toString());
-                return Token.NUMBER;
-            }
-
-            throw new RuntimeException("Invalid character: " + input.charAt(current));
+        while (Character.isWhitespace(current)) {
+            consume();
         }
 
-        return Token.EOF;
+        switch (current) {
+            case '+':
+                consume();
+                return Token.PLUS;
+
+            case '-':
+                consume();
+                return Token.MINUS;
+
+            case '*', '⊗':
+                consume();
+                return Token.MUL;
+
+            case '/':
+                consume();
+                return Token.DIV;
+
+            case '^':
+                consume();
+                return Token.Degree;
+
+            case '(':
+                consume();
+                return Token.LPAR;
+
+            case ')':
+                consume();
+                return Token.RPAR;
+        }
+
+        if (Character.isDigit(current)) {
+            StringBuilder sb = new StringBuilder();
+            do {
+                sb.append((char) current);
+                consume();
+            } while (current >= '0' && current <= '9');
+            value = Integer.parseInt(sb.toString());
+            return Token.NUMBER;
+        }
+
+        if (current == -1)
+        {
+            return Token.EOF;
+        }
+
+        throw new RuntimeException("Invalid character: " + (char) current);
     }
 
     private void consume()
     {
-        current++;
+        try
+        {
+            current = reader.read();
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 
     public int getValue() {
